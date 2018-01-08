bbbc97353a2eac63c5ba037064d1b8f57feaffdf
ArtOfCode- <hello@artofcode.co.uk>
2018-01-08 20:51:00 +0000

    Webpack config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,22 @@
-const environment = require('./environment')
+// Note: You must restart bin/webpack-dev-server for changes to take effect
 
-module.exports = environment.toWebpackConfig()
+const merge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BabiliPlugin = require('babili-webpack-plugin');
+
+const sharedConfig = require('./shared');
+
+module.exports = merge(sharedConfig, {
+  output: { filename: '[name]-[chunkhash].js' },
+  devtool: 'source-map',
+  stats: 'normal',
+
+  plugins: [
+    new BabiliPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
+    })
+  ]
+});
